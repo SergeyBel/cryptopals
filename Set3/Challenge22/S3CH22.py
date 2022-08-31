f434c2c7107e3c6edb10e59c9a7690fdcf83c915
SergeyBel <belovseregas@gmail.com>
2022-08-31 18:31:09 +0300

    S3CH22.py

---
+++ Set3/Challenge22/S3CH22.py
@@ -0,0 +1,35 @@
+import time
+import random
+import sys
+
+
+sys.path.append('../../Set3/Challenge21')
+from S3CH21 import MersenneTwister
+
+
+class MtTimeOracle:
+    def __init__(self) -> None:
+        self.rng = MersenneTwister()
+        seed = int(time.time()) + random.randint(40, 1000)
+        self.rng.seed(seed)
+
+    def getRandom(self):
+        return self.rng.getRandomNumber()
+
+if __name__ == "__main__":
+    oracle = MtTimeOracle()
+    output = oracle.getRandom()
+    timestamp = int(time.time()) + 1001
+    while timestamp > 0:
+        rng = MersenneTwister()
+        rng.seed(timestamp)
+        number = rng.getRandomNumber()
+        if  number == output:
+            print('Seed=', timestamp, number, output)
+            break
+        timestamp -= 1
+
+
+        
+
+
