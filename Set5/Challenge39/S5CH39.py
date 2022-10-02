ad0cc5ebd52365c1786fdc5ac3ded1020c7e1057
SergeyBel <belovseregas@gmail.com>
2022-10-02 14:57:09 +0300

    S5CH39.py

---
+++ Set5/Challenge39/S5CH39.py
@@ -0,0 +1,45 @@
+from Crypto.Util import number
+import math
+
+
+class Rsa:
+    def __init__(self) -> None:
+        self.e = 3
+        self.n = None
+        self.d = None
+        self.generateKeys(64)
+        
+    
+    def generateKeys(self, bits):
+        
+        while True:
+            p = number.getPrime(bits)
+            q = number.getPrime(bits)
+            if q != p:
+                phi = (p - 1) * (q - 1)
+                if math.gcd(self.e, phi) == 1:
+                    break
+        
+        self.n = p * q
+        self.d = pow(self.e, -1, phi)
+    
+    def getPublicKey(self):
+        return self.e, self.n
+    
+    def encrypt(self, number: int)->int:
+        return pow(number, self.d, self.n)
+    
+    def decrypt(self, encrypted: int)->int:
+        return pow(encrypted, self.e, self.n)
+
+
+
+
+
+if __name__ == "__main__":
+    rsa  = Rsa()
+    num = 42
+    encrypted = rsa.encrypt(num)
+    decrypted = rsa.decrypt(encrypted)
+    
+    print('Decrypt correct:', decrypted == num)
